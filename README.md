52a94fb66a0d7575ab82f7bac6470c4680cb8593
gilgamesh7 <felidaesrule@yahoo.com>
2022-10-08 21:03:20 +1300

    DB password implemnetde in Azure Key Vault

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Links
 - [Building A Simple CRUD Application With FastAPI](https://www.gormanalysis.com/blog/building-a-simple-crud-application-with-fastapi/)
 - [VDI - Swagger Documentation](http://127.0.0.1:8000/docs)
+- [Create ODBC engine fot Azure SQL in SQLAlchemy](https://docs.sqlalchemy.org/en/14/dialects/mysql.html#module-sqlalchemy.dialects.mysql.pyodbc)
 
 ## Installation
 ### Initial
@@ -10,6 +11,11 @@
 - pip install pipenv
 - pipenv install fastapi
 - pipenv install "uvicorn[standard]"
+- pipenv install pyodbc
+- pipenv install sqlalchemy
+- pipenv install azure-identity
+- pipenv install azure-keyvault-secrets
+- 
 
 ### Packages to install
 - See Pipfile
@@ -32,3 +38,16 @@ create table todolist
 	Task  NVARCHAR(128) NOT NULL
 )
 ```
+# Get access for app to Azure Keyvault
+1. Register your app with Azure Active Directory 
+    1. Azure Active Directory
+    1. App Registrations
+    1. New Registration
+    1. Name the app (fastapi-crud-exercise)
+    1. Note down for running from laptop
+        1. AZURE_CLIENT_ID
+        1. AZURE_TENANT_ID
+    1. Certificates and Secrets
+    1. New Client Secret
+    1. Give it a name (fastapi-crud-exercise)
+    1. Note down "Value" for running from laptop : AZURE_CLIENT_SECRET
--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 
 from typing import Dict
 from fastapi import FastAPI, status
+import uvicorn
+
+from sqlalchemy import create_engine,Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+import urllib
+
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+
+key_vault_name = "kv-fastapi-crud-exercise"
+vault_uri = "https://kv-fastapi-crud-exercise.vault.azure.net/"
+secret_name = "fast-api-crud-db-password"
+
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=vault_uri, credential=credential)
+
+db_password = client.get_secret(secret_name)
+print(db_password)
 
 app = FastAPI()
 
 @app.get("/", status_code=status.HTTP_200_OK)
 def root()-> Dict:
+    create_database_connection()
     return {"message":"In God We Trust!"}
 
 @app.post("/todo", status_code=status.HTTP_201_CREATED)
@@ -27,3 +47,37 @@ def delete_todo(id: int)-> str:
 @app.get("/todo")
 def read_todo_list()-> str:
     return "List of todo items"
+
+
+def create_database_connection()-> None:
+    # Create a Azure SQL(ODBC) engine instance
+    connection_string = (
+        'DRIVER=ODBC Driver 17 for SQL Server;'
+        'SERVER=tcp:sql-fastapi-crud-exercise.database.windows.net;'
+        'PORT=1433;'
+        'DATABASE=sqldb-fastapi-crud-exercise;'
+        'UID=fastapicrudadmin;'
+        f'PWD={db_password};'
+        'Encrypt=yes;'
+        'TrustServerCertificate=no;'
+        'Connection Timeout=30;'
+    )   
+    params = urllib.parse.quote_plus(connection_string)
+    connection_uri = "mysql+pyodbc:///?odbc_connect=%s" % params
+    azure_db_engine=  create_engine(connection_uri,echo=True)
+
+    # Create a DeclarativeMeta instance
+    Base = declarative_base()
+
+    # Define To Do class inheriting from Base
+    class ToDo(Base):
+        __tablename__ = 'todolist'
+        ToDoId = Column(Integer, primary_key=True)
+        Task =  Column(String(50))
+
+    # Create the database
+    Base.metadata.create_all(azure_db_engine)
+    print("Created engine")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app")
